bf54b84e4069711138da04b923cedf5d0177a721
Hani Alshehri <alshehri.hani@gmail.com>
2025-08-12 18:53:26 +0300

    feat: add version tracking for release management system integration

---
+++ version.js
@@ -0,0 +1,15 @@
+// Auto-generated version file - DO NOT EDIT MANUALLY
+module.exports = {
+  VERSION: '1.1.0-dev',
+  BUILD_DATE: '2025-01-12T00:00:00.000Z',
+  COMMIT_SHA: 'dev-latest',
+  BUILD_NUMBER: 1,
+  
+  getVersionInfo: () => ({
+    version: module.exports.VERSION,
+    buildDate: module.exports.BUILD_DATE,
+    commitSha: module.exports.COMMIT_SHA,
+    buildNumber: module.exports.BUILD_NUMBER,
+    displayVersion: `v${module.exports.VERSION} (build ${module.exports.BUILD_NUMBER})`
+  })
+};
